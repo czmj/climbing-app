755ca255cb8b87923df985aa96489d377c79728e
czmj <clara@jordan-maynard.org>
2015-04-26 15:24:53 +0100

    filter subheader

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,8 +12,8 @@ angular.module('starter.controllers', ['rzModule'])
     //range slider - rz-slider
     var gradeArray = ["1","2","3","3+","4","4+","5","5+","6a","6a+","6b","6b+","6c","6c+","7a","7a+","7b","7b+","7c","7c+","8a","8a+","8b","8b+","8c","8c+"];
     
-    $scope.gradeMax = gradeArray.length - 1;
-    $scope.gradeMin = 2;
+    $scope.gradeMax = gradeArray.length - 4;
+    $scope.gradeMin = 3;
     
     $scope.gradeTranslate = function(value)
     {
